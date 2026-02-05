942df2bba7cb51c50a1c5f882a70477e24b76889
admintechglobalsolution <admin@ntechglobalsolution.com>
2026-02-06 02:35:24 +0530

    solve prisma init during build

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,7 +1,12 @@
 import { prisma } from "@/lib/prisma";
+import { User } from "@prisma/client";
 
-export async function getUsers() {
-    return prisma.user.findMany({
-        orderBy: { createdAt: "asc" },
-    });
+export async function getUsers(): Promise<User[]> {
+  if (!prisma) {
+    return [];
+  }
+
+  return prisma.user.findMany({
+    orderBy: { createdAt: "asc" },
+  });
 }
